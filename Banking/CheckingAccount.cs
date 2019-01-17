e23236aefa021defb76f6750d4a29789aca3136d
alexanderjalbright <alexanderjalbright@gmail.com>
2019-01-17 15:59:09 -0500

    Initial Commit

---
+++ Banking/CheckingAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking
+{
+    public class CheckingAccount
+    {
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
